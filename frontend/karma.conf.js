1a72a82aae9024de55bcfd56f0f914c771017dcf
mario santos <t765668971@cassi.com.br>
2024-11-13 21:29:59 -0300

    initial scafold

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/marketplace'),
+      dir: require('path').join(__dirname, './coverage/front-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
